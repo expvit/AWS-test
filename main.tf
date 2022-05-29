9e41b9aa2456dc8199031dcbe2954919935b4fe3
vs <expvit@gmail.com>
2022-05-29 16:27:34 +0300

    bug fixing

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "ubuntu" {
   ami = "ami-0022f774911c1d690"
   instance_type = "t2.micro"
   key_name = "DevopsIBA"
-  vpc_security_group_ids = "sg-011dc925d970e5502"
+  vpc_security_group_ids = ["sg-011dc925d970e5502"]
 
   tags = {
 
