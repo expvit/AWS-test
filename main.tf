521abcb9958b195787d9792f10a394a91e3b1ff4
vs <expvit@gmail.com>
2022-05-29 16:51:26 +0300

    change main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,14 @@ provider "aws" {
   region = "us-east-1"
 }
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.75.2"
+    }
+  }
+
 resource "aws_instance" "ubuntu" {
   ami = "ami-0022f774911c1d690"
   instance_type = "t2.micro"
